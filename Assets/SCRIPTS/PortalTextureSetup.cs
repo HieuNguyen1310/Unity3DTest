f4e7305e46f9aacbfdc3741029c0fe0023f33a00
unknown <nguyentrunghieu131081@gmail.com>
2024-04-02 20:46:02 +0700

    6.1 Commit
    Change Scripts folder

--- Assets/SCRIPTS/PortalTextureSetup.cs
+++ Assets/SCRIPTS/PortalTextureSetup.cs
@@ -5,33 +5,33 @@ using UnityEngine;
 public class PortalTextureSetup : MonoBehaviour
 {   
 
-    public Camera cameraA;
-    public Camera cameraB;
+    // public Camera cameraA;
+    // public Camera cameraB;
     public Camera cameraRoom;
     public Camera cameraExit;
 
-    public Material cameraMatA;
-    public Material cameraMatB;
+    // // public Material cameraMatA;
+    // public Material cameraMatB;
     public Material cameraMatRoom;
     public Material cameraMatExit;
     // Start is called before the first frame update
     void Start()
     {
 
-        if (cameraA.targetTexture != null)
-        {
-            cameraA.targetTexture.Release();
-        }
-        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMatA.mainTexture = cameraA.targetTexture;
+        // if (cameraA.targetTexture != null)
+        // {
+        //     cameraA.targetTexture.Release();
+        // }
+        // cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        // cameraMatA.mainTexture = cameraA.targetTexture;
 
 
-        if (cameraB.targetTexture != null)
-        {
-            cameraB.targetTexture.Release();
-        }
-        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMatB.mainTexture = cameraB.targetTexture;
+        // if (cameraB.targetTexture != null)
+        // {
+        //     cameraB.targetTexture.Release();
+        // }
+        // cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        // cameraMatB.mainTexture = cameraB.targetTexture;
 
 
         if (cameraRoom.targetTexture != null)
